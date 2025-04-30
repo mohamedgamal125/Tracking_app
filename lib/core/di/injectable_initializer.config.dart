4fff1a65a8b981b3f1730860bacb895b28d47d1b
Mohamed Gamal Ragab <95649591+mohamedgamal125@users.noreply.github.com>
2025-04-30 14:33:10 +0300

    finish project initialization

---
+++ lib/core/di/injectable_initializer.config.dart
@@ -0,0 +1,38 @@
+// dart format width=80
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:dio/dio.dart' as _i361;
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
+import 'package:pretty_dio_logger/pretty_dio_logger.dart' as _i528;
+
+import '../api/network_factory.dart' as _i1013;
+
+extension GetItInjectableX on _i174.GetIt {
+// initializes the registration of main-scope dependencies inside of GetIt
+  _i174.GetIt init({
+    String? environment,
+    _i526.EnvironmentFilter? environmentFilter,
+  }) {
+    final gh = _i526.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
+    final dioProvider = _$DioProvider();
+    gh.lazySingleton<_i361.Dio>(() => dioProvider.dioProvider());
+    gh.lazySingleton<_i528.PrettyDioLogger>(() => dioProvider.providePretty());
+    gh.lazySingleton<_i1013.AuthInterceptor>(() => _i1013.AuthInterceptor());
+    return this;
+  }
+}
+
+class _$DioProvider extends _i1013.DioProvider {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:tracking_app/core/di/injectable_initializer.dart';
 import 'package:tracking_app/core/router/pages_routes.dart';
 import 'package:tracking_app/core/router/routes_generator.dart';
 import 'package:tracking_app/core/services/screen_size_service.dart';
+import 'package:tracking_app/core/services/shared_preference_services.dart';
 import 'package:tracking_app/core/utils/theming.dart';
 
+import 'core/services/bloc_observer.dart';
+import 'core/services/easy_loading_service.dart';
 import 'main_view.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  configureDependencies();
+  Bloc.observer = MyBlocObserver();
+  ConfigLoading().showLoading();
+  await SharedPreferenceServices.init();
   runApp(InitApp());
 }
 
