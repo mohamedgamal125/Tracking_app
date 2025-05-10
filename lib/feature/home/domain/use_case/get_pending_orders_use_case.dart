d5369de652c2c0b97ebc0a87d091085761ef696d
ALHassan Musleh <80463251+ALHassanMusleh@users.noreply.github.com>
2025-05-10 21:39:31 +0300

    feat(home):  - add pending orders use case.

---
+++ lib/feature/home/domain/use_case/get_pending_orders_use_case.dart
@@ -0,0 +1,12 @@
+import 'package:tracking_app/core/common/result.dart';
+import 'package:tracking_app/feature/home/domain/entites/pending_orders_response_entity.dart';
+import 'package:tracking_app/feature/home/domain/repo/pending_orders_repo.dart';
+
+class GetPendingOrdersUseCase {
+  final PendingOrdersRepo _pendingOrdersRepo;
+  GetPendingOrdersUseCase(this._pendingOrdersRepo);
+
+  Future<Result<PendingOrdersResponseEntity>> execute() async {
+    return await _pendingOrdersRepo.getPendingOrders();
+  }
+}
